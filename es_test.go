5dfa9bd45591892da48fce347ec7dc47ca22dfd4
Pham Kieu Thanh <thanhpk@live.com>
2017-09-13 23:35:29 +0700

    add publish, subscribe test

--- es_test.go
+++ es_test.go
@@ -1,11 +1,62 @@
 package kafka
 
 import (
+	"time"
 	"testing"
+	"bitbucket.org/subiz/gocommon"
+	"encoding/binary"
+	"runtime"
 )
 
 func skipTest(t *testing.T) {
-	t.Skip()
+	//t.Skip()
+}
+
+func TestPublishAndSubscribe(t *testing.T) {
+	runtime.GOMAXPROCS(400)
+	skipTest(t)
+	n := 1000
+	host := common.StartKafkaDev("")
+	const cg = "con-4345081"
+	const topic = "tmtopic43511031"//"test533124"
+	datamap := make(map[uint64]bool)
+	donechan := make(chan bool)
+
+	// subscriber
+	go func() {
+		es := &EventStore{}
+		es.Connect([]string{host}, []string{topic}, cg)
+		return
+		es.Listen(func(par int32, top string, data []byte, ofset int64) bool {
+			if topic != top {
+				return true
+			}
+			key := binary.LittleEndian.Uint64(data)
+			datamap[key] = true
+			return len(datamap) < n
+		}, func(m map[string][]int32 {
+		}))
+		es.Close()
+		donechan <- true
+	}()
+
+	// publisher
+	es := &EventStore{}
+	es.Connect([]string{host}, []string{}, "")
+	for i := uint64(0); i < uint64(n) + 1; i++ {
+		data := make([]byte, 8)
+		binary.LittleEndian.PutUint64(data, uint64(i))
+		es.Publish(topic, data)
+	}
+
+	select {
+	 case <-donechan:
+		return
+	case <-time.After(time.Second * 30):
+		t.Fatal("timeout")
+	case <-EndSignal():
+		t.Fatal("signal")
+	}
 }
 
 func TestValidateTopicRule(t *testing.T) {
