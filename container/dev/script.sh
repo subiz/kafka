7e87095f59bc5272b078153254f288c4e133ce2b
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2017-10-23 13:55:00 +0700

    add new bash util

--- container/dev/script.sh
+++ container/dev/script.sh
@@ -8,8 +8,8 @@ echo "delete.topic.enable=true" >> $KAFKA_HOME/config/server.properties
 
 
 echo "
-kafka() {
-	echo \"list, count $1, consume $1, cleartopic $1\"
+k() {
+	echo \"list, count $1, consume $1, cleartopic $1, desc $1, alter $1, $2\"
 }
 
 list() {
@@ -26,5 +26,14 @@ consume() {
 
 cleartopic() {
 	/opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic \"\$1\"
-}" >> ~/.bashrc
+}
+
+desc() {
+	./kafka-topics.sh --describe --zookeeper localhost:2181 --topic \"\$1\" | more
+}
+
+alter() {
+	/opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --alter --zookeeper localhost:2181 --topic \"\$1\" --partitions \"\$2\"
+}
+" >> ~/.bashrc
 supervisord -n
